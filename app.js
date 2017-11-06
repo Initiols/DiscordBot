59a1c218251d17f0c195d112c33ed51923b0a585
Initiols <ouinitiols@gmail.com>
2017-11-06 20:53:41 +0100

    Fixed and improved testRichEmbed command

--- app.js
+++ app.js
@@ -267,33 +267,23 @@ client.on('message', message => {
     const embed = new Discord.RichEmbed()
       .setTitle('Very Nice Title')
       .setAuthor('Author Name', 'https://goo.gl/rHndF5')
-      /*
-       * Alternatively, use '#00AE86', [0, 174, 134] or an integer number.
-       */
       .setColor(0x00AE86)
       .setDescription('The text of the body, essentially')
       .setFooter('Nice text at the bottom', 'https://goo.gl/hkFYh0')
       .setImage('https://goo.gl/D3uKk2')
       .setThumbnail('https://goo.gl/lhc6ke')
-      /*
-       * Takes a Date object, defaults to current date.
-       */
       .setTimestamp()
       .setURL('https://discord.js.org/#/docs/main/indev/class/RichEmbed')
       .addField('Field Title', 'Field Value')
-      /*
-       * Inline fields may not display as inline if the thumbnail and/or image is too big.
-       */
+      .addBlankField()
       .addField('Inline Field', 'Hmm 🤔', true)
-      /*
-       * Blank field, useful to create some space.
-       */
       .addField('\u200b', '\u200b', true)
-      .addField('Second (3rd place) Inline Field', 'I\'m in the ZOONE', true);
+      .addField('Second (3rd place) Inline Field', 'I\'m in the ZOONE', true)
+      .addField('I like those', 'still look awesome', true);
 
     message.channel.send(
-      embed,
-      'this is some content but nobody cares', {
+      'let me just write this down',
+      embed, {
         disableEveryone: true
       }
     );

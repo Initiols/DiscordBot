fb256ce81efb98be16eb8138d7b773eabc555b08
Initiols <ouinitiols@gmail.com>
2017-11-08 21:12:16 +0100

    Updated client.on('ready')

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
-  client.channels.find('name', 'bots').send('Patrick Bot s\'est connecté');
+  client.channels.find('name', 'bot').send(`${client.user.username} s\'est connecté`);
 });
 
 client.on('disconnect', () => {

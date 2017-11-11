cbec49a9d3a81cfb15065042b96639f4ff8796e5
Initiols <ouinitiols@gmail.com>
2017-11-12 00:51:09 +0100

    Added command ping

--- app.js
+++ app.js
@@ -61,6 +61,11 @@ client.on('message', message => {
 
   let devChannel = message.guild.channels.find("name", "bots"); //utiliser le config.json pour cette ligne ?
 
+  if (command === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! La latence est de ${m.createdTimestamp - message.createdTimestamp}ms. La latence de l'API est ${Math.round(client.ping)}ms`);
+  }
+
   if (command === 'messagetoembed') {
     message.channel.send(message.author,
       new Discord.RichEmbed()
